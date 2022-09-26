b51cffb98b1be7659cf3353ed14430c024dabcc9
ESCG\520522 <520522@chrome.escg.ac.uk>
2022-09-26 14:31:02 +0100

    first save of work

---
+++ first save.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string name;
+        Console.WriteLine("what is your name?");
+        name = Console.ReadLine();
+        Console.WriteLine("welcome " + name );
+
+        string battle;
+        Console.WriteLine("there's an enemy! would you like to fight?");
+        battle = Console.ReadLine();
+        if ()
+
+
+    }
+}
